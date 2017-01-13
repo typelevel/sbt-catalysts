532c5516e37b9bb564037ba42cb363dff8f88867
Kailuo Wang <kailuo.wang@gmail.com>
2017-01-13 10:48:24 -0500

    updated refined to 0.6.2

--- src/main/scala/org/typelevel/TypelevelDeps.scala
+++ src/main/scala/org/typelevel/TypelevelDeps.scala
@@ -24,7 +24,7 @@ object Dependencies {
     "macro-compat"   -> "1.1.1",
     "monocle"        -> "1.3.0",
     "paradise"       -> "2.1.0",
-    "refined"        -> "0.3.6",
+    "refined"        -> "0.6.2",
     "scalacheck"     -> "1.13.4",
     "scalatest"      -> "3.0.0",
     "scalac"         -> "2.12.1",
