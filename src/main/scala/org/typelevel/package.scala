45ff3781291335085d355db203b46f288ab487f6
Scala <me@scala-steward.org>
2019-07-12 19:59:23 +0200

    Update catalysts-checklite, ... to 0.8

--- src/main/scala/org/typelevel/package.scala
+++ src/main/scala/org/typelevel/package.scala
@@ -10,7 +10,7 @@ object `package` {
   val libraries = Libraries()
     .add(name = "algebra",          version = "1.0.1",  org = typeLevelOrg, "algebra", "algebra-laws")
     .add(name = "discipline",       version = "0.12.0-M3",  org = typeLevelOrg, "discipline-core", "discipline-scalatest")
-    .add(name = "catalysts",        version = "0.7",    org = typeLevelOrg, "catalysts-checklite", "catalysts-lawkit", "catalysts-macros", "catalysts-platform", "catalysts-scalatest", "catalysts-specbase", "catalysts-speclite", "catalysts-specs2", "catalysts-testkit")
+    .add(name = "catalysts",        version = "0.8",    org = typeLevelOrg, "catalysts-checklite", "catalysts-lawkit", "catalysts-macros", "catalysts-platform", "catalysts-scalatest", "catalysts-specbase", "catalysts-speclite", "catalysts-specs2", "catalysts-testkit")
     .add(name = "cats",             version = "2.0.0-M4",  org = typeLevelOrg, "cats-core", "cats-kernel", "cats-free", "cats-laws", "cats-macros", "cats-testkit", "alleycats-core")
     .add(name = "cats-effect",      version = "2.0.0-M4",  org = typeLevelOrg, "cats-effect", "cats-effect-laws")
     .add(name = "cats-mtl",         version = "0.5.0",  org = typeLevelOrg, "cats-mtl-core", "cats-mtl-laws")
