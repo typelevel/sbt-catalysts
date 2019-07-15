4d109237c79d2fa732c097064a463bf401914d43
Scala Steward <me@scala-steward.org>
2019-07-16 01:43:29 +0200

    Update cats-mtl-core, cats-mtl-laws to 0.6.0

--- src/main/scala/org/typelevel/package.scala
+++ src/main/scala/org/typelevel/package.scala
@@ -13,7 +13,7 @@ object `package` {
     .add(name = "catalysts",        version = "0.8",    org = typeLevelOrg, "catalysts-checklite", "catalysts-lawkit", "catalysts-macros", "catalysts-platform", "catalysts-scalatest", "catalysts-specbase", "catalysts-speclite", "catalysts-specs2", "catalysts-testkit")
     .add(name = "cats",             version = "2.0.0-M4",  org = typeLevelOrg, "cats-core", "cats-kernel", "cats-free", "cats-laws", "cats-macros", "cats-testkit", "alleycats-core")
     .add(name = "cats-effect",      version = "2.0.0-M4",  org = typeLevelOrg, "cats-effect", "cats-effect-laws")
-    .add(name = "cats-mtl",         version = "0.5.0",  org = typeLevelOrg, "cats-mtl-core", "cats-mtl-laws")
+    .add(name = "cats-mtl",         version = "0.6.0",  org = typeLevelOrg, "cats-mtl-core", "cats-mtl-laws")
     .add(name = "cats-tagless",     version = "0.9",  org = typeLevelOrg, "cats-tagless-core", "cats-tagless-laws", "cats-tagless-macros")
     .add(name = "cats-collections",  version = "0.8.0",  org = typeLevelOrg, "cats-collections-core")
     .add(name = "fs2" ,             version = "1.0.5",  org = "co.fs2", modules = "fs2-core", "fs2-io", "fs2-reactive-streams")
