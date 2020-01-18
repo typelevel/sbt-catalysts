b4568ac651a716a89a54da03c458fbe9f4e3b475
Scala Steward <me@scala-steward.org>
2020-01-18 01:49:52 +0100

    Update cats-tagless-core, ... to 0.11

--- src/main/scala/org/typelevel/package.scala
+++ src/main/scala/org/typelevel/package.scala
@@ -14,7 +14,7 @@ object `package` {
     .add(name = "cats",             version = "2.1.0",org = typeLevelOrg, "cats-core", "cats-kernel", "cats-free", "cats-laws", "cats-macros", "cats-testkit", "alleycats-core")
     .add(name = "cats-effect",      version = "2.0.0",org = typeLevelOrg, "cats-effect", "cats-effect-laws")
     .add(name = "cats-mtl",         version = "0.7.0",  org = typeLevelOrg, "cats-mtl-core", "cats-mtl-laws")
-    .add(name = "cats-tagless",     version = "0.10",    org = typeLevelOrg, "cats-tagless-core", "cats-tagless-laws", "cats-tagless-macros")
+    .add(name = "cats-tagless",     version = "0.11",    org = typeLevelOrg, "cats-tagless-core", "cats-tagless-laws", "cats-tagless-macros")
     .add(name = "cats-collections", version = "0.9.0",  org = typeLevelOrg, "cats-collections-core")
     .add(name = "fs2" ,             version = "2.1.0",  org = "co.fs2", modules = "fs2-core", "fs2-io", "fs2-reactive-streams")
     .add(name = "http4s" ,          version = "0.20.15", org = "org.http4s", modules = "http4s-dsl", "http4s-blaze-server", "http4s-blaze-client", "http4s-play-json", "http4s-circe")
