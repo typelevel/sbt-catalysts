d3582258e566f8e12d3675af13838e194583c8ba
Scala Steward <me@scala-steward.org>
2020-02-08 01:13:34 +0100

    Update cats-effect, cats-effect-laws to 2.1.1

--- src/main/scala/org/typelevel/package.scala
+++ src/main/scala/org/typelevel/package.scala
@@ -12,7 +12,7 @@ object `package` {
     .add(name = "discipline-core",  version = "1.0.2", org = typeLevelOrg)
     .add(name = "catalysts",        version = "0.8",    org = typeLevelOrg, "catalysts-checklite", "catalysts-lawkit", "catalysts-macros", "catalysts-platform", "catalysts-scalatest", "catalysts-specbase", "catalysts-speclite", "catalysts-specs2", "catalysts-testkit")
     .add(name = "cats",             version = "2.1.0",org = typeLevelOrg, "cats-core", "cats-kernel", "cats-free", "cats-laws", "cats-macros", "cats-testkit", "alleycats-core")
-    .add(name = "cats-effect",      version = "2.1.0",org = typeLevelOrg, "cats-effect", "cats-effect-laws")
+    .add(name = "cats-effect",      version = "2.1.1",org = typeLevelOrg, "cats-effect", "cats-effect-laws")
     .add(name = "cats-mtl",         version = "0.7.0",  org = typeLevelOrg, "cats-mtl-core", "cats-mtl-laws")
     .add(name = "cats-tagless",     version = "0.11",    org = typeLevelOrg, "cats-tagless-core", "cats-tagless-laws", "cats-tagless-macros")
     .add(name = "cats-collections", version = "0.9.0",  org = typeLevelOrg, "cats-collections-core")
