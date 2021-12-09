ec8f13f3e9283dfff7bfbd96c19734e191b1c79f
Scala Steward <me@scala-steward.org>
2021-12-09 04:53:19 +0100

    Update alleycats-core, cats-core, cats-free, ... to 2.7.0

--- src/main/scala/org/typelevel/package.scala
+++ src/main/scala/org/typelevel/package.scala
@@ -10,7 +10,7 @@ object `package` {
   val libraries = Libraries()
     .add(name = "algebra",          version = "2.2.3",  org = typeLevelOrg, "algebra", "algebra-laws")
     .add(name = "discipline-core",  version = "1.1.5", org = typeLevelOrg)
-    .add(name = "cats",             version = "2.6.1",org = typeLevelOrg, "cats-core", "cats-kernel", "cats-free", "cats-laws", "cats-testkit", "alleycats-core")
+    .add(name = "cats",             version = "2.7.0",org = typeLevelOrg, "cats-core", "cats-kernel", "cats-free", "cats-laws", "cats-testkit", "alleycats-core")
     .add(name = "cats-collections", version = "0.9.3",  org = typeLevelOrg, "cats-collections-core")
     .add(name = "cats-effect",      version = "3.2.2", org = typeLevelOrg, "cats-effect", "cats-effect-laws")
     .add(name = "cats-effect-testing",  version = "1.2.0",  org = typeLevelOrg, modules="cats-effect-testing-scalatest", "cats-effect-testing-utest", "cats-effect-testing-minitest")
