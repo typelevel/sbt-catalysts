38259c130596991d7f6a57d5ce3f6d1beb8e3416
Scala Steward <me@scala-steward.org>
2021-12-09 06:59:08 +0100

    Update specs2-core, specs2-mock, ... to 4.12.12

--- src/main/scala/org/typelevel/package.scala
+++ src/main/scala/org/typelevel/package.scala
@@ -36,7 +36,7 @@ object `package` {
     .add(name = "scodec",           version = "1.11.8", org = "org.scodec", "scodec-core")
     .add(name = "shapeless",        version = "2.3.7",  org = "com.chuusai")
     .add(name = "simulacrum",       version = "1.0.1", org = "org.typelevel")
-    .add(name = "specs2",           version = "4.12.4",  org = "org.specs2", "specs2-core", "specs2-scalacheck", "specs2-mock")
+    .add(name = "specs2",           version = "4.12.12",  org = "org.specs2", "specs2-core", "specs2-scalacheck", "specs2-mock")
     .add(name = "scalac"   ,        version = "2.12.14")
     .add(name = "scalac_2.13",      version = "2.13.5")
     .add(name = "scalac_2.12",      version = "2.12.14")
